b949ca567c3e062aadaa50ea8ae7f468a71858c5
3insteincomplex <myrdaproza@gmail.com>
2016-11-18 11:30:51 +1100

    fine-tuned db news

--- mknewsdb.py
+++ mknewsdb.py
@@ -19,7 +19,7 @@ newsdb = db['news_rec']
 
 
 now = datetime.datetime.now()
-past = now - datetime.timedelta(days = 1)
+past = now - datetime.timedelta(days = 11)
 start = past.strftime("%Y-%m-%d")
 end = now.strftime("%Y-%m-%d")
 
